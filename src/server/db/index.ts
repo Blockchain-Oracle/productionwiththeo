b6c4f048b56749a13cebd8c705520f86ec82b942
Blockchain Oracle <ajweb3dev@gmail.com>
2024-10-15 10:26:27 +0100

    change to supabase

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,34 +1,10 @@
-import { drizzle } from "drizzle-orm/vercel-postgres";
-import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "./schema";
-import {
-  pgTable,
-  serial,
-  text,
-  timestamp,
-  uniqueIndex,
-} from "drizzle-orm/pg-core";
+import { posts } from "./schema";
 
-// Use this object to send drizzle queries to your DB
-export const db = drizzle(sql, { schema });
-// Create a pgTable that maps to a table in your DB
-export const ExampleTable = pgTable(
-  "users",
-  {
-    id: serial("id").primaryKey(),
-    name: text("name").notNull(),
-    email: text("email").notNull(),
-    image: text("image").notNull(),
-    createdAt: timestamp("createdAt").defaultNow().notNull(),
-  },
-  (users) => {
-    return {
-      uniqueIdx: uniqueIndex("unique_idx").on(users.email),
-    };
-  },
-);
+const connectionString = process.env.DATABASE_URL;
 
-export const getExampleTable = async () => {
-  const selectResult = await db.select().from(ExampleTable);
-  console.log("Results", selectResult);
-};
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString!, { prepare: false });
+export const db = drizzle(client, { schema });
